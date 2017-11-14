f59a6afc0b8713db8429e286480491a7e26edba7
Libbie Corney <lac.goal@hotmail.com>
2017-11-14 05:51:49 -0600

    updated files

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+    <!DOCTYPE html>
+
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10px;
+                }
+                textarea {
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }
+            </style>
+        </head>
+        <body>
+            <form action="/hello" method="post">
+                <label for="rot">Rotate by:</label>
+                <input type="number" name="rot" value="0"/>
+
+                <textarea type="text" name="phrase"></textarea>
+                <input type="submit"/>
+            </form>
+
+        </body>
+    </html>
+"""
+
+
+
+@app.route("/")
+def index():
+    return form
+
+@app.route("/hello", methods=['POST'])
+def hello():
+    text = request.form['phrase']
+    num = request.form['rot']
+    num = int(num)
+
+    code = rotate_string(text, num)
+    return '<h1>' + code + '</h1>'
+
+
+
+
+
+app.run()
